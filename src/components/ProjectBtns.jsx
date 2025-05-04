b2e1060c484c6c1b6e89545a2a5ccb727ddfaa1f
Robn8 <rob.natale.8@gmail.com>
2025-05-04 17:07:05 -0500

    React projects update

--- src/components/ProjectBtns.jsx
+++ src/components/ProjectBtns.jsx
@@ -2,17 +2,17 @@ const ProjectBtns = () => {
     return (
         <div className="grid grid-cols-1 md:grid-cols-2 gap-8 ">
 
-            <a href="https://github.com/Robn8/Thelonious" target="blank">
+            <a href="https://game-two.netlify.app/" target="blank">
                 <div className="bg-gray-300 p-6 rounded-2xl shadow-md">
-                    <h3 className="text-2xl font-semibold mb-2 text-center underline">THELONIOUS</h3>
-                    <p className="text-gray-700 text-center">An app that helps musicians find chords to play based on the key that they are playing in. Developed using React Native in an ExpoGo enviroment.</p>
+                    <h3 className="text-2xl font-semibold mb-2 text-center underline">BREAKOUT</h3>
+                    <p className="text-gray-700 text-center">A browser based classic Breakout game clone. Created with the Phaser.js game framework and deployed via Netlify.</p>
                 </div>
             </a>
 
             <a href="https://ill-defined.com/" target="blank">
                 <div className="bg-gray-300 p-6 rounded-2xl shadow-md">
                     <h3 className="text-2xl font-semibold mb-2 text-center underline">ILL-DEFINED</h3>
-                    <p className="text-gray-700 text-center">A web app built on React that stores and plays music. Users can log into the web app and upload their own music and albums to the site.</p>
+                    <p className="text-gray-700 text-center">A music web application built with React, allowing users to sign in, upload their own tracks/albums and stream them directly from the platform.</p>
                 </div>
             </a>
             </div>
